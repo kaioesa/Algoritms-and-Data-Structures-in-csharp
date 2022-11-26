231a260f050ea7418d747d91f22986a4740cb2cf
Kaio Eduardo <xkaio@hotmail.com>
2022-11-26 17:46:27 -0300

    Linear search added

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AlgoritmsAndDataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var listOfNumbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            linearSearch(listOfNumbers, 6);
+        }
+
+        // Linear Search Algorithm -- O(n)
+        static void linearSearch(List<int> list, int target)
+        {
+            var result = 0;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == target)
+                {
+                    result = i + 1;
+                }
+            }
+            System.Console.WriteLine(result);
+        }
+    }
+}
