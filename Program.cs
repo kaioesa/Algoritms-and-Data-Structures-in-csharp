5ecd54da1aefd1575f4250fe772c470a2aaa88ac
Kaio Eduardo <xkaio@hotmail.com>
2022-11-27 05:04:53 -0300

    Binary Search added

--- Program.cs
+++ Program.cs
@@ -7,9 +7,15 @@ namespace AlgoritmsAndDataStructures
         static void Main(string[] args)
         {
 
-            var listOfNumbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var listOfNumbers = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var listOfNumbersUnordered = new List<int> { 78, 1, 6, 37, 96, 567, 123, 579, 23, 8, 18, 28, 58, 45, 2, 76 };
 
-            linearSearch(listOfNumbers, 6);
+            // TODO // recursiveBinarySearch(listOfNumbersUnordered, 16, 0, listOfNumbersUnordered.Count - 1);
+
+            // linearSearch(listOfNumbers, 6);
+            // binarySearch(listOfNumbers, 6);
+        
+            
         }
 
         // Linear Search Algorithm -- O(n)
@@ -26,5 +32,62 @@ namespace AlgoritmsAndDataStructures
             }
             System.Console.WriteLine(result);
         }
+
+        // Binary Search Algorithm -- 0(n log n)
+        public static void binarySearch(List<int> list, int target)
+        {
+            int first = 0;
+            int last = list.Count - 1;
+
+            while (first <= last)
+            {
+                int midpoint = (first + last) / 2;
+
+                if (list[midpoint] == target)
+                {
+                    System.Console.WriteLine($"o ponteiro está em {midpoint}");
+                    break;
+                }
+                else if (list[midpoint] < target)
+                {
+                    System.Console.WriteLine($"o ponteiro está em {midpoint}");
+                    first = midpoint - 1;
+                }
+                else
+                {
+                    System.Console.WriteLine($"o ponteiro está em {midpoint}");
+                    last = midpoint + 1;
+                }
+            }
+        }
+
+
+        // TODO
+        // public static int recursiveBinarySearch(List<int> list, int target, int min, int max)
+        // {
+        //     int midpoint = (min + max) / 2;
+
+        //     if (list[midpoint] == target)
+        //     {
+        //         foreach (int x in list)
+        //             Console.Write(x + " ");
+        //         Console.WriteLine();
+
+        //         System.Console.WriteLine($"O ponteiro está aqui {++midpoint}");
+        //         return midpoint++;
+
+        //     }
+
+        //     if (list[midpoint] > target)
+        //     {
+        //         System.Console.WriteLine($"Midpoint maior que o target, chamando função");
+        //         return recursiveBinarySearch(list, target, min, midpoint - 1);
+        //     }
+
+        //     if (max < min) return -1;
+
+        //     return recursiveBinarySearch(list, target, midpoint + 1, max);
+        //     System.Console.WriteLine($"Midpoint menor que o target, chamando função");
+        // }
     }
 }
